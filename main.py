17ae0fca6c3a6d50c2f1e43431a6316d46619bab
sashachepuha <shizadance69@gmail.com>
2020-01-10 20:09:09 +0300

    BoxLayout
    +изучил BoxLayout
    +созданы виджеты кнопок
    +начало работы над калькулятором

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.uix.codeinput import CodeInput
-from pygments.lexers import HtmlLexer
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
 
-from kivy.config import Config
-
-Config.set('graphics', 'resizable', '0')
-Config.set('graphics', 'width', '600')
-Config.set('graphics', 'height', '400')
-
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
 
 class MyApp(App):
     def build(self):
+        gl = GridLayout(cols=4, padding=[5], spacing=5)
 
-        fl = FloatLayout(size = (600, 400))
+        gl.add_widget( Button(text = '7') )
+        gl.add_widget( Button(text = '8') )
+        gl.add_widget( Button(text = '9') )
+        gl.add_widget( Button(text = 'X') )
 
-        fl.add_widget(Button(
-            text = "Button", 
-            on_press = self.btn_press,
-            background_color = [1, 0, 0, 1],
-            background_normal = '',
-            size_hint = (.5, .25),
-            pos = (50, 50)))
-        
-        return fl
+        gl.add_widget( Button(text = '4') )
+        gl.add_widget( Button(text = '5') )
+        gl.add_widget( Button(text = '6') )
+        gl.add_widget( Button(text = '-') )
 
-    def btn_press(self, instance):
-        print('кнопка нажата')
-        instance.text = 'НАЖАТА'
-if __name__=="__main__":
+        gl.add_widget( Button(text = '1') )
+        gl.add_widget( Button(text = '2') )
+        gl.add_widget( Button(text = '3') )
+        gl.add_widget( Button(text = '+') )
+
+        gl.add_widget( Widget() )
+        gl.add_widget( Button(text = '0') )
+        gl.add_widget( Button(text = ',') )
+        gl.add_widget( Button(text = '=') )
+
+        return gl
+
+if __name__ == "__main__":
     MyApp().run()
